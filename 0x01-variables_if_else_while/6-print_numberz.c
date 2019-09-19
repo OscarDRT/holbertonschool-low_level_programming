5dd68abe766973d026c60cc8ba987f0e62907027
Oscar <oscarnetworkingpro@gmail.com>
2019-09-19 22:43:43 +0000

    6. Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,11 +7,11 @@
  */
 int main(void)
 {
-  int l;
-  for (l = 48; l <= 57; ++l)
-    {
-      putchar(l);
-    }
-  putchar('\n');
-  return (0);
+int l;
+for (l = 48; l <= 57; ++l)
+{
+putchar(l);
+}
+putchar('\n');
+return (0);
 }
