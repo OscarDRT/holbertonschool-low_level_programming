ecdcf205cf2fad9615577367b23777a3531e7fcc
Oscar <oscarnetworkingpro@gmail.com>
2019-09-24 21:23:08 +0000

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * add - a + b
+ *
+ * @a: first value
+ * @b: secund value
+ *
+ * Return: c value
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
