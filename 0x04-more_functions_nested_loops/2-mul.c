429f85f4e21fb5fcfe0eee33dc3fb5210370764f
Oscar <oscarnetworkingpro@gmail.com>
2019-09-26 15:04:27 +0000

    2. Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * mul - a * b
+ *
+ * @a: first value
+ * @b: secund value
+ *
+ * Return: c value
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
