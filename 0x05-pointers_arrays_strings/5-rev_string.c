a57312dd26550661ed64b628b47373c96955690c
OscarDRT <55116114+OscarDRT@users.noreply.github.com>
2019-10-01 11:10:37 -0500

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,18 +1,32 @@
-#include "holberton.h"
-
-/**
- *
- */
 void rev_string(char *s)
 {
-	int a, b;
-
+	int a, b, tam, tmp;
+	char *fin, *inicio;
+    
+    a = 0;
+    tam = 0;
+    tmp = 0;
 	while (s[a] != '\0')
 	{
 		a++;
+		tam = a;
 	}
-	for (; a >= 0; a--)
+	
+	fin = s;
+	inicio = s;
+	
+	for (b = 0; b < tam -1; b++)
 	{
-		for (b = 0; b <= 
+        fin++;
+	}   
+	
+	for (b = 0; b < tam / 2; b++)
+	{
+	    tmp = *inicio;
+	    *inicio = *fin;
+	    *fin = tmp;
+	    
+	    inicio++;
+	    fin--;
 	}
 }
