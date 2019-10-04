3a87423363feb7c1815458fb7af2c29bf66b029b
Oscar <oscarnetworkingpro@gmail.com>
2019-10-04 00:45:00 +0000

    7. Mozart composed his music not for the elite, but for everybody

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * leet - function that encodes a string into 1337
+ *
+ * @p: function parameter
+ *
+ * Return: Always p
+ */
+char *leet(char *p)
+{
+	int a, b;
+	char ini[] = "AaEeTtLlOo";
+	char fin[] = "4433771100";
+
+	for (a = 0; p[a] != '\0'; a++)
+	{
+		for (b = 0; ini[b] = '\0'; b++)
+		{
+			if (p[a] == ini[b])
+			{
+				p[a] == fin[j];
+			}
+		}
+	}
+	return (p);
+}
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -12,4 +12,6 @@
 
 5. Always look up
 
+6. Expect the best. Prepare for the worst. Capitalize on what comes
 
+7. Mozart composed his music not for the elite, but for everybody
