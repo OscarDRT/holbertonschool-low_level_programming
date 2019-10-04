f3933e96474257169a0b1d6740a5c337405e81bf
OscarDRT <55116114+OscarDRT@users.noreply.github.com>
2019-10-03 22:00:41 -0500

    Update 8-rot13.c

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -1,9 +1,27 @@
 #include "holberton.h"
 
 /**
+ * rot13 - function that encodes a string into rot13
  *
+ * @p: function parameter
+ *
+ * Return: Always p
  */
-char *rot13(char *)
+char *rot13(char *p)
 {
-
+	int a, b;
+	
+	char ma[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char mi[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	for (a = 0; p[a] != '\0'; a++)
+	{
+		for (b = 0; ma[b] != '\0'; b++)
+		{
+			if (p[a] == ma[b])
+			{
+				p[a] = mi[b];
+			}
+		}
+	}
+	return (p);
 }
