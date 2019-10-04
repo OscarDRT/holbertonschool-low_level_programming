a012b07df2145ffb7ec44a14237480fd09498939
OscarDRT <55116114+OscarDRT@users.noreply.github.com>
2019-10-03 22:43:01 -0500

    Update 8-rot13.c

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -7,21 +7,22 @@
  *
  * Return: Always p
  */
-char *rot13(char *p)
+char *rot13(char *s)
 {
 	int a, b;
 	char ma[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char mi[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	for (a = 0; p[a] != '\0'; a++)
+	for (a = 0; s[a] != '\0'; a++)
 	{
 		for (b = 0; ma[b] != '\0'; b++)
 		{
-			if (p[a] == ma[b])
+			if (s[a] == ma[b])
 			{
-				p[a] = mi[b];
+				s[a] = mi[b];
+				break;
 			}
 		}
 	}
-	return (p);
+	return (s);
 }
