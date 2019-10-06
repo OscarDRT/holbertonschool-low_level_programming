715335ced37f8c7ffdbafc73876a29e8f72be708
OscarDRT <55116114+OscarDRT@users.noreply.github.com>
2019-10-06 15:40:06 -0500

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,37 +7,27 @@
  */
 int main(void)
 {
-int i;
-int j;
-int k;
-int n;
-int m;
-int l;
-n = '2';
-m = '1';
-l = '2';
-for (i = '0' ; i <= '7'; i++)
-{
-for (j = '1' ; j <= '9'; j++)
-{
-for (k = '2' ; k <= '9'; k++)
-{
-putchar(i);
-putchar(j);
-putchar(k);
-if (i == '7' && j == '8' && k == '9')
-break;
-putchar(',');
-putchar(' ');
-}
-n++;
-k = n;
-}
-m++;
-j = m;
-l++;
-k = l;
-}
-putchar('\n');
-return (0);
+	int a, b, c;
+	
+	for (a = '0'; a <= '7'; a++)
+	{
+		for (b = '1'; b <= '8'; b++)
+		{
+			for (c = '2'; c <= '9'; c++)
+			{
+				if (a < b && a < c && b < c)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(c);
+					if ((a >= '0' && a <= '6') )
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
 }
