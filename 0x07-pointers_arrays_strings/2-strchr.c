74f0ce4e8a64ec1bbb5ab9bbbfc25f196c1a83d9
Oscar <oscarnetworkingpro@gmail.com>
2019-10-07 18:46:44 +0000

    change point 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,13 +12,14 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	p = s;
-	for (i = 0; s[i] != '\0'; i++)
+	while ( s[i] != c)
 	{
-		if (s[i] == c)
+		i++;
+		s++;
+		if (s[i] == NULL)
 		{
-			return (s + i);
+			return (NULL);
 		}
 	}
-	return (s + i);
+	return (s);
 }
