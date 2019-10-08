861ea61492499fe2c4fc29a398934705a71c014b
Oscar <oscarnetworkingpro@gmail.com>
2019-10-08 14:18:12 +0000

    change

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -12,8 +12,12 @@ void print_diagsums(int *a, int size)
 	int con, sumad1 = 0, sumad2 = 0;
 
 	for (con = 0; con < (size * size); con += size + 1)
+	{
 		sumad1 = sumad1 + a[con];
+	}
 	for (con = size - 1; con < (size * size) - 1; con += size - 1)
+	{
 		sumad2 = sumad2 + a[con];
+	}
 	printf("%d, %d\n", sumd1, sumad2);
 }
