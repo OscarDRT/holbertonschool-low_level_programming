7293325adc3a7715cee3424b505e3c1ae872e8b4
OscarDRT <55116114+OscarDRT@users.noreply.github.com>
2019-10-08 00:00:17 -0500

    Update 8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,17 +9,10 @@
 */
 void print_diagsums(int *a, int size)
 {
-	int con, sumad1, sumad2;
-
-	sumad1 = 0;
-	sumad2 = 0;
-	for (con = 0; i < size * size; con += (size + 1))
-	{
-		sumad1 += a[con];
-	}
-	for (con = size - 1; i < (size * size) - (size - 1); con += (size - 1))
-	{
-		sumad2 += a[con];
-	}
-	printf("%d, %d\n", sumad1, sumad2);
+	int con, sumad1 = 0, sumad2 = 0;
+	for (con = 0; con < (size * size); con += size + 1)
+		sumad1 = sumad1 + a[con];
+	for (con = size - 1; con < (size * size) - 1; con += size - 1)
+		sumad2 = sumad2 + a[con];
+	printf("%d, %d\n", sumd1, sumad1);	
 }
