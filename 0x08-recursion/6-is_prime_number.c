882ad754ef6f390275ff7e9db7fd03f3886befd1
oscar <oscarnetworkingpro@gmail.com>
2019-10-10 02:38:57 +0000

    6. Inception. Is it possible?

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -3,17 +3,37 @@
 /**
  *
  */
-vooid help(int *mov
+void help(int *ptr, int *pt, int x)
+{
+	if (x % (*ptr) == 0)
+	{
+		*pt = 0;
+		return;
+	}
+	if (x % (*ptr) != 0 && *ptr <= x / 2)
+	{
+		*ptr = *ptr + 1;
+		*pt = 1;
+		help(ptr, pt, x);
+
+	}
+}
 
 /**
 *
 */
 int is_prime_number(int n)
 {
+	int ret = 0;
+	int *pt = &ret;
+	int mov = 2;
+	int *ptr = &mov;
 	
+	help(ptr, pt, n);
+	ret = *pt;
 	if (n <= 1)
 	{
 		return (0);
 	}
-
+	return (ret);
 }
