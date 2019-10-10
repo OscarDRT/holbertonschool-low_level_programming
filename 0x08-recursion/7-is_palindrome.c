045d9254fbadf735ecb9eec7339539a839dd36e2
oscar <oscarnetworkingpro@gmail.com>
2019-10-10 03:19:26 +0000

    prueba

---
+++ 0x08-recursion/7-is_palindrome.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ *
+ */
+void help(char *inicio, char *fin, int *re, int lon)
+{
+	if
+}
+
+/**
+ *
+ */
+int is_palindrome(char *s)
+{
+	int lon = 0;
+	int ret = 0;
+	int *re = &ret;
+	int *fin;
+	int *inicio;
+
+	fin = s;
+	if(*s)
+	{
+		lon = lon + 1;
+		fin++;
+	}
+	help(s, fin, re, lon);
+
+}
