076591339132ff2c39deabe782e16031add5417e
oscar <oscarnetworkingpro@gmail.com>
2019-10-15 16:31:27 -0500

    0. Float like a butterfly, sting like a bee

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,18 +2,23 @@
 #include <stdlib.h>
 
 /**
-*
-*/
+ * create_array - function for assignament dinamic memory
+ *
+ * @size: size for parameter
+ * @c: character for parameter
+ *
+ * Return: 0 if size is 0 or ptr point null, other case ptr
+ */
 char *create_array(unsigned int size, char c)
 {
 	char *ptr;
 	unsigned int i;
-	
-	if ( size == 0 size == NULL)
+
+	ptr = malloc(size * sizeof(char));
+	if (size == 0 || ptr == NULL)
 	{
 		return (NULL);
 	}
-	ptr = malloc(size * sizeof(char));
 	for (i = 0; i < size; i++)
 	{
 		ptr[i] = c;
