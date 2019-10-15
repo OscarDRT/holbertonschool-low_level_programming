18f891bb4b6f829a43112b2c75c1d53447dfa4a2
oscar <oscarnetworkingpro@gmail.com>
2019-10-15 15:53:55 -0500

    proof

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+*
+*/
+char *create_array(unsigned int size, char c)
+{
+	char *ptr;
+	unsigned int i;
+	
+	if ( size == 0 size == NULL)
+	{
+		return (NULL);
+	}
+	ptr = malloc(size * sizeof(char));
+	for (i = 0; i < size; i++)
+	{
+		ptr[i] = c;
+	}
+	return (ptr);
+}
