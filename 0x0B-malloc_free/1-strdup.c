704f25acd64e3f58d9d6bcc3b56b02447da12fb6
oscar <oscarnetworkingpro@gmail.com>
2019-10-15 17:42:36 -0500

    1. The woman who has no imagination has no wings

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - contains a copy of the string given as a parameter
+ *
+ * @str: string send for parameter
+ *
+ * Return: NULL or copy
+ */
+char *_strdup(char *str)
+{
+	int i, tam = 0;
+	char *copy;
+	while (str[tam] != '\0')
+	{
+		tam++;
+	}
+	tam++;
+	copy = malloc(tam * sizeof(char));
+	if (str == NULL || copy == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < tam; i++)
+	{
+		copy[i] = str[i];
+	}
+	return (copy);
+}
