1d34c538cf0d60fac5424a2560c6356105a4f9bd
oscar <oscarnetworkingpro@gmail.com>
2019-10-16 15:25:51 -0500

    send

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,7 +15,7 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	ptr = malloc(height * sizeof(int *));
+	ptr = (int **)malloc(height * sizeof(int *));
 	if (ptr == NULL)
 		return (NULL);
 	for (i = 0; i < height; i++)
