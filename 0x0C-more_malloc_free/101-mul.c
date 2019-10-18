22eb301e4713cbcef2866898ec215239d435f6ef
Oscar Riano Tapias <oscarnetworkingpro@gmail.com>
2019-10-18 18:34:16 -0500

    send

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,7 +2,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * _strlen - count the number of characters
+ *
+ * @s: function parameter
+ *
+ * Return: Always cont
+ */
+int _strlen(char *s)
+{
+	int cont;
 
+	cont = 0;
+	while (s[cont] != '\0')
+	{
+		cont++;
+	}
+	return (cont);
+}
+
+/**
+ * error - print Error
+ */
 void error()
 {
 	int i;
@@ -14,12 +35,13 @@ void error()
 	}
 	exit(98);
 }
+
 /**
 *
 */
 int main(int argc, char *argv[])
 {
-	int i;
+	int i, tn1, tn2;
 	char *ptr;
 
 	if (argc != 3)
@@ -38,6 +60,8 @@ int main(int argc, char *argv[])
 			ptr++;
 		}
 	}
-	
+	tn1 = _strlen(argv[1]);
+	tn2 = _strlen(argv[2]);
+	printf("%d %d", tn1, tn2);
 	return (0);
 }
