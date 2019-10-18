d26f93e9256afe9db97ce7af55cefae2a9882eae
oscar <oscarnetworkingpro@gmail.com>
2019-10-17 21:25:00 -0500

    5 reallocccccc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -10,26 +10,24 @@
  * Return: NULL or ptr or p
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
-{
-	unsigned int i, tam;
+
+	unsigned int tam, i;
 	char *p;
 
-	if (new_size == old_size)
+	if (old_size == new_size)
 	{
 		return (ptr);
 	}
-	if (new_size == 0 && ptr != NULL)
+	if (old_size < new_size)
+	{
+		tam = old_size;
+	}
+	else
+		tam = new_size;
+	if (new_size == 0)
 	{
 		free(ptr);
 		return (NULL);
-	}
-	if (new_size > old_size)
-	{
-		tam = old_size,
-	}
-	else
-	{
-		tam = new_size,
 	}
 	p = malloc(new_size);
 	if (p == NULL)
