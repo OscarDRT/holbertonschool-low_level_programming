27388457f4590bbc11c48730ecb1fbfcf281a120
oscar <oscarnetworkingpro@gmail.com>
2019-10-17 19:34:30 -0500

    fast

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -34,7 +34,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 	if (ptr == NULL)
 	{
-		return (malloc(new_size));
+		return (p);
 	}
 	for (i = 0; i < new_size; i++)
 	{
