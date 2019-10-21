366d504abda1184e7206611373f7142fb0a5b95b
Oscar Riano Tapias <oscarnetworkingpro@gmail.com>
2019-10-21 12:34:47 -0500

    222

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,11 +2,11 @@
 
 /**
  * main - Print our owner name
- *
+ * @void: Whit out parameter
  * Return: Always 0.
  */
 int main(void)
 {
-	printf("%s\n", __FILE__ );
-        return (0);
+	printf("%s\n", __FILE__);
+	return (0);
 }
