b3b594895ec885eca7cad3a5e7e5b86e4c6bc9ba
Oscar Riano Tapias <oscarnetworkingpro@gmail.com>
2019-10-21 14:16:40 -0500

    0. Django

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,24 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - struct for puppy
+ * @name: name of puppy
+ * @age: age of puppy
+ * @owner: name owner of puppy
+ *
+ * Description: struct that contains some information about the puppy
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog - Typedef for struct dog
+ */
+typedef struct dog dog;
+
+#endif
