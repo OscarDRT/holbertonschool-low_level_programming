18dbd14a8890f684ad09118eb503de250a74a774
Oscar Riano Tapias <oscarnetworkingpro@gmail.com>
2019-10-23 13:12:26 -0500

    1. If you spend too much time thinking about a thing, you'll never get it done

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,7 +1,7 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator - executes a function given as a parameter on each element of an array
+ * array_iterator - function given as a parameter on each element of an array
  * @array: send for parameter
  * @size: size of the array
  * @action: s a pointer to the function
