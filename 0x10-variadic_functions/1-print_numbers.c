ae0622c71b11d4693ed94730936ebcadbe08d619
Oscar Riano Tapias <oscarnetworkingpro@gmail.com>
2019-10-26 15:25:52 -0500

    dfh

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -20,7 +20,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		for (i = 0; i < n; i++)
 		{
-			if (i == (n - 1))
+			if (i == n - 1)
 			{
 				printf("%d", va_arg(valist, int));
 				break;
