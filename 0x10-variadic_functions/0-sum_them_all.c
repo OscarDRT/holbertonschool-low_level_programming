7d59f649c622212944e54120b0da06fad8b0937f
oscar <oscarnetworkingpro@gmail.com>
2019-10-27 15:19:05 -0500

    change 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -3,13 +3,12 @@
 /**
  * sum_them_all - function that returns the sum of all its parameters
  * @n: number of parameter send for user
- * Return: Alway sum
+ * Return: Alway sum of parameters
  */
 int sum_them_all(const unsigned int n, ...)
 {
+	unsigned int i, sum = 0;
 	va_list valist;
-	unsigned int sum = 0;
-	unsigned int i;
 
 	if (n == 0)
 		return (0);
