55aa5db845b7ef89d5932e91cacfa081144f850e
Oscar Riano Tapias <oscarnetworkingpro@gmail.com>
2019-11-05 09:27:17 -0500

    a

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,7 +8,7 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	if (*head == NULL)
+	if (*head == NULL || head == NULL)
 		return;
 	while (*head != NULL)
 	{
@@ -16,6 +16,5 @@ void free_listint2(listint_t **head)
 		*head = (*head)->next;
 		free(temp);
 	}
-	free(*head);
 	*head = NULL;
 }
