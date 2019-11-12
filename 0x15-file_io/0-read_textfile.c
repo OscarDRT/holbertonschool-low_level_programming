5ac7f386b7348c2036bd93d188489f9140908f87
Oscar Riano Tapias <oscarnetworkingpro@gmail.com>
2019-11-12 14:05:59 -0500

    a

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,18 +8,21 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd, save;
-	char buff[1024];
+	int fd, sav1, sav2;
+	char *buff;
 
+	buff = malloc(letters);
 	if (filename == NULL)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	save = read(fd, buff, letters);
-	if (save == -1)
+	sav1 = read(fd, buff, letters);
+	if (sav1 == -1)
 		return (0);
 	close(fd);
-	write(STDOUT_FILENO, buff, save);
-	return (save);
+	sav2 = write(STDOUT_FILENO, buff, sav1);
+	if (sav2 == -1)
+		return (0);
+	return (sav1);
 }
