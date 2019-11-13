7064fda5204f7c917c5635a21cdd98a433b8b4ec
oscar <oscarnetworkingpro@gmail.com>
2019-11-12 20:57:22 -0500

    program that copies the content of a file to another file.

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,44 +8,33 @@
  */
 int main(int argc, char *argv[])
 {
-	int fd, fdnew, aux, aux2 = 0;
+	int fd_O, fd_D, aux;
 	char buff[1024];
-	mode_t perm = S_IRUSR | S_IWUSR | S_IWGRP | S_IRGRP | S_IROTH;
+	int per = S_IRUSR | S_IWUSR | S_IWGRP | S_IRGRP | S_IROTH;
 
 	if (argc != 3)
 		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]), exit(97);
-	fd = open(argv[1], O_RDONLY);
-	if (fd == -1)
+	fd_O = open(argv[1], O_RDONLY);
+	if (fd_O == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	fdnew = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, perm);
-	if (fdnew == -1)
+	fd_D = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, per);
+	if (fd_D == -1)
 	{
 		dprintf(STDERR_FILENO, "EError: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
-	aux = 1024;
-	while (aux == 1024)
+	while ((aux = read(fd_O, buff, 1024)) > 0)
 	{
-		aux = read(fd, buff, 1024);
-		if (aux == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			exit(98);
-		}
-		aux2 = write(fdnew, buff, aux);
-		if (aux2 < aux)
+		if (write(fd_D, buff, aux) != aux)
 		{
 			dprintf(STDERR_FILENO, "EError: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
 	}
-
-	if (close(fd) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %s\n", argv[1]), exit(100);
-	if (close(fdnew) == -1)
+	if ((close(fd_O) == -1) || (close(fd_D) == -1))
 		dprintf(STDERR_FILENO, "Error: Can't close fd %s\n", argv[1]), exit(100);
 	return (0);
 }
