cd7ed3e99bb59d6747f32f4e17f92fd74eb25d03
oscar <oscarnetworkingpro@gmail.com>
2020-01-18 18:57:29 -0500

    function that adds an element to the hash table.

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -34,6 +34,5 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new_node->value = strdup(value);
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
-	printf("Direccion: %p\n", (void *)ht->array[index]);
 	return (1);
 }
