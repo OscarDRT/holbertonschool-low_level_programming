91f5710894f874558dde05bb01cfed5335bfb215
oscar <oscarnetworkingpro@gmail.com>
2020-01-18 22:02:01 -0500

    Change frre(new node )

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -27,6 +27,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (ht->array[index] != NULL && (strcmp(key, ht->array[index]->key) == 0))
 	{
 		free(ht->array[index]->value);
+		free(new_node);
 		ht->array[index]->value = strdup(value);
 		return (1);
 	}
