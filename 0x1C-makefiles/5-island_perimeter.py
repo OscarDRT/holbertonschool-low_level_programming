15675cef2aefbc26a881b074278530c8186c72ae
Oscar Riano Tapias <oscarnetworkingpro@gmail.com>
2020-02-21 18:51:07 -0500

    change

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -2,9 +2,9 @@
 
 def island_perimeter(grid):
     suma = 0
-    if len(grid) =< 100:
+    if len(grid) <= 100:
         for i in range(len(grid)):
-            if len(grid[i]) =< 100:
+            if len(grid[i]) <= 100:
                 for j in range(len(grid[i])):
                     if grid[i][j] == 1:
                         perimeter = 4
