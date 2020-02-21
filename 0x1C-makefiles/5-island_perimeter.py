646a2fb0187d0d718adf9219f4feb3064bcc4e80
Oscar Riano Tapias <oscarnetworkingpro@gmail.com>
2020-02-21 18:43:10 -0500

    change

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -2,30 +2,31 @@
 
 def island_perimeter(grid):
     suma = 0
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-            perimeter = 4
-            if grid[i][j] == 1:
-                try:
-                    if grid[i][j + 1] == 1:
-                        perimeter -= 1
-                except IndexError:
-                    pass
-                try:
-                    if grid[i][j - 1] == 1:
-                        perimeter -= 1
-                except IndexError:
-                    pass
-                try:
-                    if grid[i + 1][j] == 1:
-                        perimeter -= 1
-                except IndexError:
-                    pass
-                try:
-                    if grid[i - 1][j] == 1:
-                        perimeter -= 1
-                except IndexError:
-                    pass
-                suma += perimeter
-
+    if len(grid) < 100:
+        for i in range(len(grid)):
+            if len(grid[i]):
+                for j in range(len(grid[i])):
+                    perimeter = 4
+                    if grid[i][j] == 1:
+                        try:
+                            if grid[i][j + 1] == 1:
+                                perimeter -= 1
+                        except IndexError:
+                            pass
+                        try:
+                            if grid[i][j - 1] == 1:
+                                perimeter -= 1
+                        except IndexError:
+                            pass
+                        try:
+                            if grid[i + 1][j] == 1:
+                                perimeter -= 1
+                        except IndexError:
+                            pass
+                        try:
+                            if grid[i - 1][j] == 1:
+                                perimeter -= 1
+                        except IndexError:
+                            pass
+                        suma += perimeter
     return suma
